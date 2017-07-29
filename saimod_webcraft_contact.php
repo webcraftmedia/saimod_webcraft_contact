2935cd6494b37ccf51362ccfd5c90e87f339e0f8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2017-07-30 00:51:19 +0200

    menu active ability

--- saimod_webcraft_contact.php
+++ saimod_webcraft_contact.php
@@ -5,7 +5,7 @@ class saimod_webcraft_contact extends \SYSTEM\SAI\SaiModule {
         return \SYSTEM\PAGE\replace::replaceFile((new PSAI('saimod_webcraft_contact/tpl/saimod_webcraft_contact.tpl'))->SERVERPATH(),$vars);
     }
     
-    public static function html_li_menu(){return '<li><a href="#!contact"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Contact</a></li>';}
+    public static function html_li_menu(){return '<li id="menu_contact"><a href="#!contact"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Contact</a></li>';}
     public static function right_public(){return false;}    
     public static function right_right(){return true;}
     public static function js(){
