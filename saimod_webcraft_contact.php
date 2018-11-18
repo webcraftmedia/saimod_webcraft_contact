5a17f893dd809d5152f3dc6b91cb351dfcaa03fa
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-11-18 22:22:31 +0100

    adjusted to new system api

--- saimod_webcraft_contact.php
+++ saimod_webcraft_contact.php
@@ -1,11 +1,13 @@
 <?php
-class saimod_webcraft_contact extends \SYSTEM\SAI\SaiModule {
+class saimod_webcraft_contact extends \SYSTEM\SAI\sai_module {
     public static function sai_mod_saimod_webcraft_contact(){
         $vars = array('project' => \SYSTEM\CONFIG\config::get(\SYSTEM\CONFIG\config_ids::SYS_SAI_CONFIG_PROJECT));
         return \SYSTEM\PAGE\replace::replaceFile((new PSAI('saimod_webcraft_contact/tpl/saimod_webcraft_contact.tpl'))->SERVERPATH(),$vars);
     }
     
-    public static function html_li_menu(){return '<li id="menu_contact"><a href="#!contact"><span class="glyphicon glyphicon-envelope"></span>&nbsp;&nbsp;Contact</a></li>';}
+    public static function menu(){
+        return new \SYSTEM\SAI\sai_module_menu( 10, \SYSTEM\SAI\sai_module_menu::POISITION_RIGHT, \SYSTEM\SAI\sai_module_menu::DIVIDER_RIGHT,
+                        \SYSTEM\PAGE\replace::replaceFile((new PSAI('saimod_webcraft_contact/tpl/menu.tpl'))->SERVERPATH()));}
     public static function right_public(){return false;}    
     public static function right_right(){return true;}
     public static function js(){
---
+++ tpl/menu.tpl
@@ -0,0 +1,7 @@
+<li class="nav-item">
+    <a id="menu_contact" class="nav-link" data-toggle="tooltip" title="Contact" href="#!contact">
+        <span class="d-lg-none" style="padding-left: 15px;"></span>
+        <i class="fa fa-envelope" aria-hidden="true"></i>
+        <span class="d-lg-none">&nbsp;&nbsp;Contact</span>
+    </a>
+</li>
